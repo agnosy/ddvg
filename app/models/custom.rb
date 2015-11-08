4d228938e21cdd8e62c62a1bb094f935b7a011cd
X Nagireddy <xnagireddy@users.noreply.github.com>
2015-11-08 17:28:55 +0530

    Explicitly getting the id for campaign and state in custom.

--- app/models/custom.rb
+++ app/models/custom.rb
@@ -2,10 +2,10 @@ class Custom < ActiveRecord::Base
   belongs_to :state
   belongs_to :next, class_name: "State"
   def to_vxml
-    campaign_id = State.find(self.state).campaign
+    campaign_id = State.find(self.state).campaign.id
     return <<-EOS
     <block>
-      <goto next="#{self.url}?next=/campaign/index/#{campaign_id}&amp;state=state#{self.next}"/>
+      <goto next="#{self.url}?next=/campaign/index/#{campaign_id}&amp;state=state#{self.next.id}"/>
     </block>
     EOS
   end
