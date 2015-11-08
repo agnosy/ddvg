063bb7d41a8afced6ab01bbb955d04a7da878d82
X Nagireddy <xnagireddy@users.noreply.github.com>
2015-11-08 16:33:49 +0530

    Removed the files generated by the scaffolding.

--- app/controllers/campaign_controller.rb
+++
@@ -1,8 +0,0 @@
-class CampaignController < ApplicationController
-  layout false
-
-  def index
-    #@campaign = Campaign.find(980190962)
-    @campaign = Campaign.first
-  end
-end
--- app/views/campaigns/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @campaign, :id, :name, :description, :start_state_id, :created_at, :updated_at
